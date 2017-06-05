3048a198921fdc47300f7855cd30b4aeb9d852c6
Terrance Niechciol <TNiechciol@gmail.com>
2017-06-05 18:53:14 -0400

    Basic board and movement

--- src/main.c
+++ src/main.c
@@ -5,12 +5,16 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <math.h>
+#include <memory.h>
 
 #include "shader.h"
 #include "math/mat4.h"
 #include "vertex_data.h"
 
 #define PI 3.14159
+
+#define copyArray(arr1, arr2) memcpy(arr1, arr2, sizeof(arr2))
 
 int triangleVAO;
 int cubeVAO;
@@ -55,31 +59,51 @@ float tetronimo4[] = {
 };
 
 typedef struct {
-  float* base_tetronimo;
+  float tetronimo[16];
   int size;
   float angle;
   float targetAngle;
+  int x;
+  int y;
 } TetronimoRep;
+
+TetronimoRep activeTet;
 
 TetronimoRep tet_rep;
 TetronimoRep tet_rep2;
 TetronimoRep tet_rep3;
 TetronimoRep tet_rep4;
 
-void rotate_tet_rep(TetronimoRep *rep) {
-  rep->angle -= PI/2;
-  mat4_transposen_in_place((mat4*)rep->base_tetronimo, rep->size);
-  mat4_flip_vertn_in_place((mat4*)rep->base_tetronimo, rep->size);
+float constrain(float val, float low, float high) {
+  val = fmaxf(val, low);
+  val = fminf(val, high);
+  return val;
 }
 
-void draw_tet_rep(TetronimoRep *rep, int modelMatrixLocation, float xOffset, float yOffset) {
-  int size = rep->size;
+void update_tet_rotation(TetronimoRep *tet) {
+  float diffAngle = tet->targetAngle - tet->angle;
+  tet->angle += constrain(diffAngle/4, 0.05, diffAngle);
+}
+
+void rotate_tet_rep(TetronimoRep *tet) {
+  tet->angle -= PI/2;
+  mat4_transposen_in_place((mat4*)(&tet->tetronimo), tet->size);
+  mat4_flip_vertn_in_place((mat4*)(&tet->tetronimo), tet->size);
+}
+
+void draw_tet_rep(TetronimoRep *tet, int modelMatrixLocation) {
+  int size = tet->size;
+  float xOffset = tet->x;
+  float yOffset = tet->y;
+  xOffset -= size/2.0;
+  yOffset -= size/2.0;
   for (int i = 0; i < 16; i++) {
-    if (rep->base_tetronimo[i] == 0) continue;
+    if (tet->tetronimo[i] == 0) continue;
     mat4 translate = mat4_translate((i/4)+0.5-(size/2.0), (3-(i%4))+0.5-(4-size)-(size/2.0), 0);
-    mat4 tetRot = mat4_rotateZ(rep->angle);
+    mat4 tetRot = mat4_rotateZ(tet->angle);
     mat4 mat = mat4_multiply(&tetRot, &translate);
-    translate = mat4_translate(size/2.0+xOffset, size/2.0+yOffset, 0);
+    //translate = mat4_translate(size/2.0+xOffset, size/2.0+yOffset, 0.25);
+    translate = mat4_translate(size/2.0-0.5+tet->x, size/2.0-0.5+tet->y, 0.25);
     mat = mat4_multiply(&translate, &mat);
     glUniformMatrix4fv(modelMatrixLocation, 1, false, &mat.values[0]);
     glDrawArrays(GL_TRIANGLES, 0, cubeBufElems/3);
@@ -106,6 +130,19 @@ void one_iter() {
         rotate_tet_rep(&tet_rep2);
         rotate_tet_rep(&tet_rep3);
         rotate_tet_rep(&tet_rep4);
+        rotate_tet_rep(&activeTet);
+      }
+
+      if (keyEvent->type == SDL_KEYDOWN && keyEvent->keysym.sym == SDLK_DOWN) {
+        activeTet.y -= 1;
+      }
+
+      if (keyEvent->type == SDL_KEYDOWN && keyEvent->keysym.sym == SDLK_LEFT) {
+        activeTet.x -= 1;
+      }
+
+      if (keyEvent->type == SDL_KEYDOWN && keyEvent->keysym.sym == SDLK_RIGHT) {
+        activeTet.x += 1;
       }
 
       if (keyEvent->type == SDL_KEYDOWN && keyEvent->keysym.sym == SDLK_r) {
@@ -141,57 +178,81 @@ void one_iter() {
     int colorLocation = glGetUniformLocation(flatShader.program, "u_color");
 
     {
-      mat4 mat = mat4_translate(-1.5, -2.0, -8);
+      mat4 mat = mat4_translate(0, -10, -17);
       glUniformMatrix4fv(viewMatrixLocation, 1, false, &mat.values[0]);
+    }
+    {
+      mat4 persp = mat4_perspective(PI/180 * 70, 640.0/480.0, 0.1, 100.0);
+      glUniformMatrix4fv(projectionMatrixLocation, 1, false, &persp.values[0]);
     }
 
     {
       glBindVertexArray(cubeVAO);
 
-      mat4 rot;
-      //rot = mat4_rotateY(0.03);
-      //cubeModelMatrix = mat4_multiply(&cubeModelMatrix, &rot);
-      //rot = mat4_rotateX(0.01);
-      //cubeModelMatrix = mat4_multiply(&cubeModelMatrix, &rot);
-      //rot = mat4_rotateZ(0.02);
-      //cubeModelMatrix = mat4_multiply(&cubeModelMatrix, &rot);
-      mat4 persp = mat4_perspective(PI/180 * 70, 640.0/480.0, 0.1, 100.0);
-      glUniformMatrix4fv(projectionMatrixLocation, 1, false, &persp.values[0]);
-      for (int j = 0; j < 4; j++) {
-        float xOffset = 3;
-        float yOffset = -3;
-        float r = 1.0;
-        float g = 0.0;
-        float b = 0.0;
-        TetronimoRep* tet = &tet_rep;
-        if (j % 2 == 0) {
-          xOffset = -xOffset;
-        }
-        if (j / 2 == 0) {
-          yOffset = -yOffset;
-        }
-        if (j == 1) {
-          tet = &tet_rep2;
-          r = 0;
-          g = 1;
-        }
-        if (j == 2) {
-          tet = &tet_rep3;
-          r = 0;
-          b = 1;
-        }
-        if (j == 3) {
-          tet = &tet_rep4;
-          r = 0;
-          g = 1;
-          b = 1;
-        }
-        float diffAngle = tet->targetAngle - tet->angle;
-        tet->angle += diffAngle/4;
+      {
+        mat4 rot;
+        //rot = mat4_rotateY(0.03);
+        //cubeModelMatrix = mat4_multiply(&cubeModelMatrix, &rot);
+        //rot = mat4_rotateX(0.01);
+        //cubeModelMatrix = mat4_multiply(&cubeModelMatrix, &rot);
+        //rot = mat4_rotateZ(0.02);
+        //cubeModelMatrix = mat4_multiply(&cubeModelMatrix, &rot);
+        for (int j = 0; j < 4; j++) {
+          int xOffset = 3;
+          int yOffset = 6;
+          float r = 1.0;
+          float g = 0.0;
+          float b = 0.0;
+          TetronimoRep* tet = &tet_rep;
+          if (j % 2 == 0) {
+            xOffset = -xOffset;
+          }
+          if (j / 2 == 0) {
+            yOffset = 0;
+          }
+          if (j == 1) {
+            tet = &tet_rep2;
+            r = 0;
+            g = 1;
+          }
+          if (j == 2) {
+            tet = &tet_rep3;
+            r = 0;
+            b = 1;
+          }
+          if (j == 3) {
+            tet = &tet_rep4;
+            r = 0;
+            g = 1;
+            b = 1;
+          }
+          update_tet_rotation(tet);
 
-        glUniform3f(colorLocation, r, g, b);
+          glUniform3f(colorLocation, r, g, b);
 
-        draw_tet_rep(tet, modelMatrixLocation, xOffset, yOffset);
+          tet->x = xOffset-2;
+          tet->y = yOffset;
+
+          draw_tet_rep(tet, modelMatrixLocation);
+        }
+      }
+
+      {
+        update_tet_rotation(&activeTet);
+
+        glUniform3f(colorLocation, 1, 1, 1);
+        draw_tet_rep(&activeTet, modelMatrixLocation);
+      }
+
+      {
+        for (int i = 0; i < 20; i++) {
+          for (int j = 0; j < 10; j++) {
+            glUniform3f(colorLocation, 0.8, 0.8, 0.8);
+            mat4 mat = mat4_translate(j - 5, i, 0);
+            glUniformMatrix4fv(modelMatrixLocation, 1, false, &mat.values[0]);
+            glDrawArrays(GL_TRIANGLES, 0, cubeBufElems/3);
+          }
+        }
       }
     }
   }
@@ -230,22 +291,29 @@ int main() {
   mat4_transpose_in_place((mat4*)tetronimo3);
   mat4_transpose_in_place((mat4*)tetronimo4);
 
-  tet_rep.base_tetronimo = tetronimo;
+  copyArray(activeTet.tetronimo, tetronimo);
+  activeTet.size = 3;
+  activeTet.angle = 0;
+  activeTet.targetAngle = 0;
+  activeTet.x = 0 - activeTet.size/2;
+  activeTet.y = 22 - activeTet.size;
+
+  copyArray(tet_rep.tetronimo, tetronimo);
   tet_rep.size = 3;
   tet_rep.angle = 0;
   tet_rep.targetAngle = 0;
 
-  tet_rep2.base_tetronimo = tetronimo2;
+  copyArray(tet_rep2.tetronimo, tetronimo2);
   tet_rep2.size = 4;
   tet_rep2.angle = 0;
   tet_rep2.targetAngle = 0;
 
-  tet_rep3.base_tetronimo = tetronimo3;
+  copyArray(tet_rep3.tetronimo, tetronimo3);
   tet_rep3.size = 3;
   tet_rep3.angle = 0;
   tet_rep3.targetAngle = 0;
 
-  tet_rep4.base_tetronimo = tetronimo4;
+  copyArray(tet_rep4.tetronimo, tetronimo4);
   tet_rep4.size = 3;
   tet_rep4.angle = 0;
   tet_rep4.targetAngle = 0;
